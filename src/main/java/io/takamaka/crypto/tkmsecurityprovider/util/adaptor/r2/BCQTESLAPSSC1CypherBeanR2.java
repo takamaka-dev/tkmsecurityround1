2c7687568ca78b59512725e5b960b780c7c8b4f9
giovanni.antino@h2tcoin.com <giovanni.antino@h2tcoin.com@poldo.h2tcoin.com>
2020-09-04 19:29:58 +0200

    tkmsecurityround1.git versione 0.1

---
+++ src/main/java/io/takamaka/crypto/tkmsecurityprovider/util/adaptor/r2/BCQTESLAPSSC1CypherBeanR2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.takamaka.crypto.tkmsecurityprovider.util.adaptor.r2;
+
+/**
+ * Round 2 result beans
+ *
+ * @author giovanni.antino@h2tcoin.com
+ */
+public class BCQTESLAPSSC1CypherBeanR2 {
+
+    private boolean valid;
+    private String signature;
+    private Exception ex;
+
+    public Exception getEx() {
+        return ex;
+    }
+
+    protected void setEx(Exception ex) {
+        this.ex = ex;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    protected void setValid(boolean valid) { 
+        this.valid = valid;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    protected void setSignature(String signature) {
+        this.signature = signature;
+    }
+}

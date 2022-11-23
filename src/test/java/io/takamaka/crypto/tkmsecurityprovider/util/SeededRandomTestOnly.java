7d208cb44ff4c312244bfe8143d950d6cab60a7e
zeikeneim <alessandro.pasi@takamaka.io>
2022-11-23 16:21:51 +0100

    Moved SeededRandomTestOnly to to util package

--- src/test/java/io/takamaka/crypto/tkmsecurityprovider/util/SeededRandomTestOnly.java
+++ src/test/java/io/takamaka/crypto/tkmsecurityprovider/util/SeededRandomTestOnly.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package io.takamaka.crypto.tkmsecurityprovider.util.adaptor.r2;
+package io.takamaka.crypto.tkmsecurityprovider.util;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
